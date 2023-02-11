058f119ec26ea37312ca3f5179c4af5e1fc83201
aditya5700 <97225489+aditya5700@users.noreply.github.com>
2023-02-11 14:31:54 +0530

    Add files via upload

---
+++ Notes-App/9.notes-app/notes-app/app.js
@@ -0,0 +1,63 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+
+yargs.version('1.1.0')
+
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
+
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+
+yargs.parse()
