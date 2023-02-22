eb824725367b2281e9ec261bc2c01294d4e09ae4
aditya5700 <97225489+aditya5700@users.noreply.github.com>
2023-02-22 17:01:59 +0530

    Add files via upload

---
+++ Javascript_practice/01/script.js
@@ -0,0 +1,19 @@
+let js = 'amazing';
+console.log(20+30+10);
+
+console.log("Aditya");
+console.log (23);
+
+let firstName = "Aditya";
+console.log(firstName);
+
+let myFirstjob ="Programmer";
+console.log(myFirstjob);
+
+let country = "India";
+let continent = "Asia";
+let population = 1500000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
