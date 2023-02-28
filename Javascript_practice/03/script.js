0d8b308f77155272ed7c66947f9c1c0dee573924
aditya5700 <97225489+aditya5700@users.noreply.github.com>
2023-02-28 12:17:16 +0530

    Add files via upload

---
+++ Javascript_practice/03/script.js
@@ -0,0 +1,111 @@
+// let js = 'amazing';
+// console.log(20+30+10);
+
+// console.log("Aditya");
+// console.log (23);
+
+// let firstName = "Aditya";
+// console.log(firstName);
+
+// let myFirstjob ="Programmer";
+// console.log(myFirstjob);
+
+// let country = "India";
+// let continent = "Asia";
+// let population = 1500000000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// const now = 2040;
+// const ageAditya = now - 2000;
+// const agePrachi = now - 1990;
+// console.log(ageAditya, agePrachi);
+
+// console.log(ageAditya * 2, agePrachi / 2, agePrachi + ageAditya, 2**3);
+
+// const firstName = 'Aditya';
+// const lastName = 'Yadav';
+// console.log( firstName + ' ' + lastName);
+
+
+// let x = 10+ 5;
+// x += 10;
+// x *= 4;
+// x ++;
+// x --;
+// x --;
+// console.log(x);
+
+// console.log(ageAditya > agePrachi);
+// console.log(agePrachi >= 80);
+
+// const isFullAge = agePrachi >= 80;
+
+// console.log (now - 1991 > now - 2022);
+
+// const now = 2040;
+// const ageAditya = now - 2000;
+// const agePrachi = now - 1990;
+
+// console.log (now - 1991 > now - 2010);
+// let x, y;
+// x = y = 25-10-5;
+// console.log(x, y);
+
+// const averageAge = (ageAditya + agePrachi) / 2
+// console.log (ageAditya, agePrachi, averageAge);
+
+
+//First Condition
+// let weightMark = 78;
+// let weightJohn = 92;
+// let heightMark = 1.69;
+// let heightJohn = 1.95;
+
+// const BMI = weightMark / (heightMark * heightMark);
+// console.log( BMI);
+
+//second condition
+
+let weightMark = 95;
+let weightJohn = 85;
+let heightMark = 1.88;
+let heightJohn = 1.76;
+
+const bmiMark = weightMark / (heightMark * heightMark);
+console.log( bmiMark);
+const bmiJohn = weightJohn / (heightJohn * heightJohn);
+console.log( bmiJohn);
+
+console.log( bmiMark > bmiJohn);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (hour >8){
+//     greeting = "Good Morning";
+// }
+// else if (hour >18{
+//     greetingh = "good night";
+// }
+
+// else {
+//     greeting ="good evening";
+// }
